1b3cf7f6de9bf7448bc3c9a467060ecb3a9d0bcc
nima <nima.bm@gmail.com>
2023-09-19 13:53:26 +0330

    update to 1.0.3

--- define.php
+++ define.php
@@ -1,5 +1,5 @@
 <?php
-define('ZANJIR_PLUGIN_VERSION', '1.0.1');
+define('ZANJIR_PLUGIN_VERSION', '1.0.3');
 define('ZANJIR_PLUGIN_PATH', plugin_dir_path(__FILE__));
 define('ZANJIR_PLUGIN_URL', plugin_dir_url(__FILE__));
 ?>
